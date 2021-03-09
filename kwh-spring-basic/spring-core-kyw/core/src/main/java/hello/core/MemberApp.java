f6970fa323f361a2744381d1d14d0814ea27033a
TaeHoonLee <robi02@naver.com>
2021-03-09 23:09:10 +0900

    스프링 핵심 원리 - 기본편 : 순수 자바로 서비스 구현.

---
+++ kwh-spring-basic/spring-core-kyw/core/src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,25 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+
+public class MemberApp {
+    
+    /**
+     * 순수 자바로 테스트.
+     */
+    public static void main(String[] args) {
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+
+        System.out.println("newMember = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
